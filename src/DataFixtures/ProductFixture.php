cb9caf6c015f3a6a4714c611c26f198adf7dc231
Abdelhakim Ayed <abdelhakim.ayed@insat.rnu.tn>
2025-06-02 00:40:36 +0100

    product+category fixtures

---
+++ src/DataFixtures/ProductFixture.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpKernel\KernelInterface;
+use App\Service\UploaderService;
+
+use App\Entity\Product;
+
+class ProductFixture extends Fixture
+{
+    private $kernel;
+    private $uploaderService;
+
+    public function __construct(KernelInterface $kernel, UploaderService $uploaderService)
+    {
+        $this->kernel = $kernel;
+        $this->uploaderService = $uploaderService;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->loadCategories($manager);
+        $this->loadProductsOfCategory($manager, "labtop");
+        $this->loadProductsOfCategory($manager, "computer");
+        $this->loadProductsOfCategory($manager, "monitor");
+        $this->loadProductNoCategory($manager, "mouse");
+        $this->loadProductNoCategory($manager, "keyboard");
+
+    }
+
+    public function loadCategories(ObjectManager $manager): void
+    {
+        $categories = ["computer", "labtop", "monitor"];
+        foreach ($categories as $catName) {
+            $category = new Category();
+            $category->setName($catName);
+            $imageName = "cat_" . $catName . '.png';
+            $imagePath = $this->kernel->getProjectDir() . "/dummy-data/" . $catName . '/' . $imageName;
+            $url = $this->uploaderService->copy($imagePath);
+            $category->setImageURL($url);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+
+    public function loadProductsOfCategory(ObjectManager $manager, string $cat_name): void
+    {
+        $faker = \Faker\Factory::create();
+        $price_min = 100;
+        $price_max = 9000;
+
+        for ($i = 0; $i < 30; $i++) {
+            $product = new Product();
+            $product->setTitle($faker->words(3, true));
+            $product->setDescription($faker->words(rand(0, 12), true));
+            $product->setPrice(rand($price_min, $price_max));
+
+            $properties = [];
+            for ($j = 0; $j < rand(2, 5); $j++) {
+                $properties[$faker->word] = rand(1, 100);
+            }
+            $product->setProperties($properties);
+
+            $category = $manager->getRepository(Category::class)->findOneBy(['name' => $cat_name]);
+            if ($category) {
+                $product->setCategory($category);
+            }
+
+            $image_name = $cat_name . '_' . rand(1, 6) . '.png';
+            $imagePath = $this->kernel->getProjectDir() . '/dummy-data/' . $cat_name . '/' . $image_name;
+
+            if (file_exists($imagePath)) {
+                $url = $this->uploaderService->copy($imagePath);
+                $product->setImageURL($url);
+            }
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+
+    }
+
+    public function loadProductNoCategory(ObjectManager $manager, string $prod_name): void
+    {
+        $faker = \Faker\Factory::create();
+        $price_min = 100;
+        $price_max = 9000;
+
+        for ($i = 0; $i < 5; $i++) {
+            $product = new Product();
+            $product->setTitle($faker->words(3, true));
+            $product->setDescription($faker->words(rand(0, 12), true));
+            $product->setPrice(rand($price_min, $price_max));
+
+            $properties = [];
+            for ($j = 0; $j < rand(2, 5); $j++) {
+                $properties[$faker->word] = rand(1, 100);
+            }
+            $product->setProperties($properties);
+
+            $image_name = $prod_name . '.png';
+            $imagePath = $this->kernel->getProjectDir() . '/dummy-data/' . $image_name;
+
+            if (file_exists($imagePath)) {
+                $url = $this->uploaderService->copy($imagePath);
+                $product->setImageURL($url);
+            }
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+
+}
