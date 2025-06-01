b1259085fdbb11864a56780548ec9ac8ee6293e5
ademby <adem.benyarou10@gmail.com>
2025-06-01 21:16:33 +0100

    Working on UI

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,14 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/cart")
- */
+#[Route('/cart')]
 final class CartController extends AbstractController
 {
-    /**
-     * @Route("/", name="cart_show")
-     */
+    #[Route('/', name: 'cart_show')]
     public function index(): Response
     {
         return $this->render('cart/index.html.twig', [
@@ -21,17 +17,13 @@ final class CartController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/add", name="cart_add")
-     */
+    #[Route("/add", name: "cart_add")]
     public function add(): Response
     {
         return new Response("Added Product To Cart");
     }
 
-    /**
-     * @Route("/validate", name="cart_validate")
-     */
+    #[Route("/validate", name: "cart_validate")]
     public function validate(): Response
     {
         return new Response("Are you sure ? ... <br> We sent you an email, our agent will talk to you soon.");
